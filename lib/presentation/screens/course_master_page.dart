b16fda2116f2b528c5b8ca3880718088e5c0c6d4
Your Naclse <you@example.com>
2021-10-05 22:52:49 +0500

    Course master page

---
+++ lib/presentation/screens/course_master_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CourseMasterDetailPage extends StatefulWidget {
+  static const routeName = '/courseMaster';
+  const CourseMasterDetailPage({Key? key}) : super(key: key);
+
+  @override
+  _CourseMasterDetailPageState createState() => _CourseMasterDetailPageState();
+}
+
+class _CourseMasterDetailPageState extends State<CourseMasterDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
